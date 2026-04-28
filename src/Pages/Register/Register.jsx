436ba97fd1e5c4d9838d9b409ed4ea949cd27182
Isabela Ohana <isabelaohanadev@gmail.com>
2026-04-28 12:39:09 -0300

    Profissão ADVOGADO adicionada ao registro

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -312,6 +312,7 @@ const Register = () => {
               >
                 <MenuItem value="Psicólogo">Psicólogo(a)</MenuItem>
                 <MenuItem value="Arquiteto">Arquiteto(a)</MenuItem>
+                <MenuItem value="Arquiteto">Advogado(a)</MenuItem>
               </Select>
             </FormControl>
             {/* MENSAGEM DE ERRO DA PROFISSÃO */}
