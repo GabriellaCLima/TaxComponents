93e376a705458dd7a96ef81f103bdbdd1975bb80
GabriellaCLima <gabriellacrisostomo@hotmail.com>
2026-06-08 21:45:32 -0300

    atualizacao imports server js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+import express from 'express';
+import cors from 'cors';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
